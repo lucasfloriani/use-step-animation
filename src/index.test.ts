305106c108c2acb74ea2d03361103a1f39a31824
Lucas Floriani <lucas.floriane@gmail.com>
2022-01-04 18:55:10 -0300

    feat: add tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,48 +1,39 @@
 import { useStepAnimation } from ".";
 import { renderHook, act } from "@testing-library/react-hooks";
-import { useRef } from "react";
-// import { Globals } from "react-spring";
 
-// Globals.assign({
-//   skipAnimation: true,
-// });
-
-// mock timer using jest
 jest.useFakeTimers();
-jest.mock("react", () => {
-  const originReact = jest.requireActual("react");
-  const mUseRef = jest.fn();
-  return {
-    ...originReact,
-    useRef: mUseRef,
-  };
-});
+jest.mock("react-spring", () => ({
+  ...jest.requireActual("react-spring"),
+  useSpring: (value: object) => value,
+}));
+jest.mock("./util", () => ({
+  ...jest.requireActual("./util"),
+  sleep: (value: object) =>
+    new Promise((resolve) => {
+      resolve(value);
+    }),
+}));
 
 const SECOND = 1000;
+const mockStep = (x = 0, y = 0): any => ({
+  current: {
+    getBoundingClientRect() {
+      return { x, y };
+    },
+  },
+});
 
 describe("useStepAnimation", () => {
-  beforeEach(() => {
-    jest.useFakeTimers();
-  });
+  beforeEach(() => jest.useFakeTimers());
+  afterEach(() => jest.useRealTimers());
 
-  afterEach(() => {
-    jest.useRealTimers();
-  });
-
-  it("updates every second", () => {
-    const callback = jest.fn();
-    const { result } = renderHook(() => {
-      const mockStep = (x = 0, y = 0) => ({
-        getBoundingClientRect() {
-          callback();
-          return { x, y };
-        },
-      });
-      const firstStepRef = useRef<any>(mockStep(100, 100));
-      const stepTwoRef = useRef<any>(mockStep(200, 200));
-      const stepThreeRef = useRef<any>(mockStep(320, 50));
-      const stepFourRef = useRef<any>(mockStep(475, 300));
-      const stepFiveRef = useRef<any>(mockStep(600, 30));
+  it("should update state correctly after each action is executed", async () => {
+    const { result, waitForNextUpdate } = renderHook(() => {
+      const firstStepRef = mockStep(100, 100);
+      const stepTwoRef = mockStep(200, 200);
+      const stepThreeRef = mockStep(320, 50);
+      const stepFourRef = mockStep(475, 300);
+      const stepFiveRef = mockStep(600, 30);
 
       return useStepAnimation({
         stepsRef: [
@@ -55,13 +46,107 @@ describe("useStepAnimation", () => {
       });
     });
 
-    act(() => result.current.nextStep());
+    await waitForNextUpdate();
+
+    expect(result.current.animationProps.top).toBe(100);
+    expect(result.current.animationProps.left).toBe(100);
+    expect(result.current.step).toBe(0);
+    expect(result.current.stepsPositions).toEqual([
+      { x: 100, y: 100 },
+      { x: 200, y: 200 },
+      { x: 320, y: 50 },
+      { x: 475, y: 300 },
+      { x: 600, y: 30 },
+    ]);
+
     act(() => {
+      result.current.nextStep();
       jest.advanceTimersByTime(SECOND);
     });
-    console.log("callback.mock.calls.length:", callback.mock.calls.length);
-    expect(callback.mock.calls.length).toBe(5);
-    expect(result.current.animationProps.top).toBe("100px");
-    expect(result.current.animationProps.left).toBe("100px");
+
+    expect(result.current.animationProps.top).toBe(200);
+    expect(result.current.animationProps.left).toBe(200);
+    expect(result.current.step).toBe(1);
+
+    act(() => {
+      result.current.nextStep();
+      jest.advanceTimersByTime(SECOND);
+    });
+
+    expect(result.current.animationProps.top).toBe(50);
+    expect(result.current.animationProps.left).toBe(320);
+    expect(result.current.step).toBe(2);
+
+    act(() => {
+      result.current.nextStep();
+      jest.advanceTimersByTime(SECOND);
+    });
+
+    expect(result.current.animationProps.top).toBe(300);
+    expect(result.current.animationProps.left).toBe(475);
+    expect(result.current.step).toBe(3);
+
+    act(() => {
+      result.current.nextStep();
+      jest.advanceTimersByTime(SECOND);
+    });
+
+    expect(result.current.animationProps.top).toBe(30);
+    expect(result.current.animationProps.left).toBe(600);
+    expect(result.current.step).toBe(4);
+
+    act(() => {
+      result.current.nextStep();
+      jest.advanceTimersByTime(SECOND);
+    });
+
+    expect(result.current.animationProps.top).toBe(30);
+    expect(result.current.animationProps.left).toBe(600);
+    expect(result.current.step).toBe(4);
+
+    act(() => {
+      result.current.previousStep();
+      jest.advanceTimersByTime(SECOND);
+    });
+
+    expect(result.current.animationProps.top).toBe(300);
+    expect(result.current.animationProps.left).toBe(475);
+    expect(result.current.step).toBe(3);
+
+    act(() => {
+      result.current.previousStep();
+      jest.advanceTimersByTime(SECOND);
+    });
+
+    expect(result.current.animationProps.top).toBe(50);
+    expect(result.current.animationProps.left).toBe(320);
+    expect(result.current.step).toBe(2);
+
+    act(() => {
+      result.current.previousStep();
+      jest.advanceTimersByTime(SECOND);
+    });
+
+    expect(result.current.animationProps.top).toBe(200);
+    expect(result.current.animationProps.left).toBe(200);
+    expect(result.current.step).toBe(1);
+
+    act(() => {
+      result.current.previousStep();
+      jest.advanceTimersByTime(SECOND);
+    });
+
+    expect(result.current.animationProps.top).toBe(100);
+    expect(result.current.animationProps.left).toBe(100);
+    expect(result.current.step).toBe(0);
+
+    act(() => {
+      result.current.previousStep();
+      jest.advanceTimersByTime(SECOND);
+    });
+
+    expect(result.current.animationProps.top).toBe(100);
+    expect(result.current.animationProps.left).toBe(100);
+    expect(result.current.step).toBe(0);
   });
 });
--- src/util.tsx
+++ src/util.tsx
@@ -2,6 +2,6 @@ export const sleep = (ms: number) =>
   new Promise((resolve) => setTimeout(resolve, ms));
 
 export const getPositionByRef = (ref: React.RefObject<HTMLDivElement>) => {
-  const { x, y } = ref.current?.getBoundingClientRect() ?? { x: 0, y: 0 };
+  const { x, y } = ref?.current?.getBoundingClientRect() ?? { x: 0, y: 0 };
   return { x, y };
 };
